513a9efe004801a4d0da831d1bb0563c983f0f2c
Pierosebas8 <pierosebas8@hotmail.com>
2025-12-07 17:51:11 -0500

    Version final

---
+++ inventario/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- Usuario: admin@inventario.com
+-- Pass: 12345
+INSERT IGNORE INTO usuario (id, nombre, correo, contrasena) VALUES 
+(1, 'Admin', 'admin@inventario.com', '$2a$12$CTcqM1anNiG24QpoySVRgukwEwCatZgQU6EpCjoWYka4mEa1Lv6/y');
